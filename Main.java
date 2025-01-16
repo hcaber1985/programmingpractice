cb80ae27c91e97dc3f40c728f31472e23ba1125e
Adle Jean-Paul <119632859+hcaber1985@users.noreply.github.com>
2025-01-16 10:04:36 -0500

    Simplify the code into one larger While Loop
    Used one larger while loop to fix the code and also added a continue statement
    to the conditional that checks if the number is valid. It was skipping over the longer condition if statements and adding the original value
    which was 0 for gradescore

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main{
         System.out.println("Hello World");
 
         //Ok so I need to  first find a way to prompt the user to give me their list of grades and stop when they are done
+        int gradescore = 0;
 
         while (true) { 
             System.out.println("Points");
@@ -21,56 +22,43 @@ public class Main{
             int finalgrades = Integer.valueOf(finalGrade);
            if(finalgrades<0 || finalgrades>100){
                 System.out.println("Impossible number.");
-            }else{
-                grades.add(finalgrades);
+                continue;
             }
-        }System.out.println(grades);
-        int grade0 = 0;
-        int grade1 = 0;
-        int grade2 = 0;
-        int grade3 = 0;
-        int grade4 = 0;
-        int grade5 = 0;
-
-        for (int i = 0; i< grades.size(); i++){
-            int currentgrade = grades.get(i);
-            if(currentgrade>=90 && currentgrade<=100 ){
-                grade5++;
-            }else if(currentgrade>=80 && currentgrade<=89 ){
-                grade4++;
-            }else if(currentgrade>=70 && currentgrade<=79 ){
-                grade3++;
-            }else if(currentgrade>=60 && currentgrade<=69 ){
-                grade2++;
-            }else if(currentgrade>=50 && currentgrade<=59 ){
-                grade1++;
-            }else if(currentgrade<50){
-                grade0++;
+            if(finalgrades>=90 && finalgrades<=100 ){
+                gradescore = 5;
+            }else if(finalgrades>=80 && finalgrades<=89 ){
+                gradescore = 4;
+            }else if(finalgrades>=70 && finalgrades<=79 ){
+                gradescore = 3;
+            }else if(finalgrades>=60 && finalgrades<=69 ){
+                gradescore = 2;
+            }else if(finalgrades>=50 && finalgrades<=59 ){
+                gradescore = 1;
+            }else if(finalgrades<50 && finalgrades>=0){
+                gradescore = 0;
             }
-        }System.out.println(grade5 +" "+ grade4+" "+ grade3+" "+ grade2+" "+ grade1+" "+ grade0);
-        while(grade5>0){
-            System.out.print("*");
-            grade5--;
-            
-        }System.out.println("");while(grade4>0){
-            System.out.print("*");
-            grade4--;
-             
-        }System.out.println("");while(grade3>0){
-            System.out.print("*");
-            grade3--;
-             
-        }System.out.println("");while(grade2>0){
-            System.out.print("*");
-            grade2--;
-            
-        }System.out.println("");while(grade1>0){
-            System.out.print("*");
-            grade1--;
-             
-        }System.out.println("");while(grade0>0){
-            System.out.print("*");
-            grade0--;
+            grades.add(gradescore);
+            System.out.println(grades);
+    
         }
+        int grade = 5; //sets initial value for grade
+        while (grade>=0){ //this while loop will circle through all the grades possible from 0-5
+            int stars = 0;//this controls the amount of stars that are printed. Created outside the for loop because otherwise it would reset the value of the stars everytime it moves to another value in the array list
+            for(int i = 0; i<grades.size();i++){//cycles through the grades array list
+                int received = grades.get(i);//assigns the index to an actual value so we can compare
+                if(received == grade){//if the particular exam score corresponds to the number we are looking for like 5 for example it will add another star. basically keeps track of how many times a grade appears
+                    stars++;//adds another star so again keeps track of how many times someone got over 90 for example
+                }//we leave the if loop but stars still has the value that we iterated on in the if and for loop
+            }System.out.println();
+            System.out.print(grade+": ");// key point at this point the stars variable and the grade variable have not changed 
+            while(stars>0){
+                System.out.print("*");
+                stars--;//stars is decremented so that we print only the correct amount
+            }grade--;//grade is decremented so that we can move onto the next possible score
+        }System.out.println("");
+
+
+        
+        
     }   
 }
